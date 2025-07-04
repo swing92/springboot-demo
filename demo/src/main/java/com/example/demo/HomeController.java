b7299d6622a8fa7f738d1617cd540e64958a9fb3
swing92 <146159769+swing92@users.noreply.github.com>
2025-07-04 17:14:19 +0000

    스프링부트 환경세팅 연습

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // templates/index.html 을 찾아서 렌더링함
+    }
+}
